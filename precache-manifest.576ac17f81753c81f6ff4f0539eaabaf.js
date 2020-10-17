1b3ffa14cd4b5a208eb6b14a3343ab754039f960
volbok <59297702+volbok@users.noreply.github.com>
2020-10-16 22:18:43 -0300

    Updates

---
+++ precache-manifest.576ac17f81753c81f6ff4f0539eaabaf.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c235fa98e4aaa5e3405ecc02dc9a59b6",
+    "url": "/pulsar-web/index.html"
+  },
+  {
+    "revision": "85cef05cae3dac9990bc",
+    "url": "/pulsar-web/static/css/2.33edbbf8.chunk.css"
+  },
+  {
+    "revision": "5449385ae652e84d378f",
+    "url": "/pulsar-web/static/css/main.af020ec1.chunk.css"
+  },
+  {
+    "revision": "85cef05cae3dac9990bc",
+    "url": "/pulsar-web/static/js/2.fb5e5fdc.chunk.js"
+  },
+  {
+    "revision": "ff754369631398eb2bd0ce2b4436d8af",
+    "url": "/pulsar-web/static/js/2.fb5e5fdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5449385ae652e84d378f",
+    "url": "/pulsar-web/static/js/main.daf2adbf.chunk.js"
+  },
+  {
+    "revision": "9f45725cf592ea3d5065",
+    "url": "/pulsar-web/static/js/runtime-main.ea45924b.js"
+  },
+  {
+    "revision": "09a2e32ab04b28f09c35c10a063481e9",
+    "url": "/pulsar-web/static/media/body.09a2e32a.png"
+  },
+  {
+    "revision": "fa0d653b38f4b8bfacab5a77720a2625",
+    "url": "/pulsar-web/static/media/deletar.fa0d653b.svg"
+  },
+  {
+    "revision": "6910013d0ea8bb4b3276a0853e24f1f3",
+    "url": "/pulsar-web/static/media/editar.6910013d.svg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/pulsar-web/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/pulsar-web/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/pulsar-web/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/pulsar-web/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/pulsar-web/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "a32e2d2e5398dfe408bf20beb355a42b",
+    "url": "/pulsar-web/static/media/novo.a32e2d2e.svg"
+  },
+  {
+    "revision": "9fdb9d7fade84d8bc5611804c491dcfe",
+    "url": "/pulsar-web/static/media/pulsar.9fdb9d7f.svg"
+  },
+  {
+    "revision": "c843937c4b376b17f53ff735342c1a7d",
+    "url": "/pulsar-web/static/media/salvar.c843937c.svg"
+  }
+]);
